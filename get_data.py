99534c787cdf2022398488fec3484ad4dc38a361
yushuodev <77037575+yushuodev@users.noreply.github.com>
2024-04-08 17:43:04 +0800

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ import argparse
 
 def download_candlesticks(ticker):
     crawler = finlab_crypto.crawler
-    ohlcv = crawler.get_all_binance(symbol='ticker', kline_size='1m')
+    ohlcv = crawler.get_all_binance(symbol=ticker, kline_size='1m')
     
     ohlcv.reset_index(inplace=True)
     ohlcv['Date'] = pd.to_datetime(ohlcv['timestamp']).dt.date
@@ -13,11 +13,9 @@ def download_candlesticks(ticker):
     ohlcv.set_index('Date', inplace=True)
     ohlcv.to_csv(f'./data/{ticker}_1m.csv')
 
-download_candlesticks()
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ticker', type=str, default="ETHUSDT", help="Symbol that will download")
+    parser.add_argument('--ticker', type=str, default="BTCUSDT", help="Symbol that will download")
 
     args = parser.parse_args()
     coin = args.ticker
